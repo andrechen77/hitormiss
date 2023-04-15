391f6cafb68b601b49fab109489de8a9896acf67
andrechen77 <andrexchen@gmail.com>
2023-04-15 14:16:04 -0500

    remove top pill bar

--- SargeHitOrMiss/App.tsx
+++ SargeHitOrMiss/App.tsx
@@ -18,15 +18,13 @@ export default function App() {
         },
       ]}
       isStaticPills={false}
+      style={swiperStyles}
     />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const swiperStyles = {
+  pillContainer: {
+    display: "none",
   },
-});
+};
